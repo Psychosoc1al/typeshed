60d0670475c58b68feb5f77cdcb76c353d46bd1c
Psychosoc1al <tohichpetrov@gmail.com>
2026-08-31 01:37:22 +0300

    Type-hint `codec/cer/`

--- stubs/pyasn1/pyasn1/codec/cer/decoder.pyi
+++ stubs/pyasn1/pyasn1/codec/cer/decoder.pyi
@@ -1,37 +1,56 @@
 from _typeshed import Unused
+from collections.abc import Generator
+from typing import Any, Literal, TypeVar, overload
 
+from pyasn1 import error
 from pyasn1.codec.ber import decoder
-from pyasn1.type import univ
-from pyasn1.type.tag import TagSet
+from pyasn1.codec.streaming import _ReadSeekable
+from pyasn1.type import base, tag, univ
 
 __all__ = ["decode", "StreamingDecoder"]
 
+_T = TypeVar("_T", bound=base.Asn1Type)
+
 class BooleanPayloadDecoder(decoder.AbstractSimplePayloadDecoder):
     protoComponent: univ.Boolean
+
+    @overload
     def valueDecoder(
         self,
-        substrate,
-        asn1Spec,
-        tagSet: TagSet | None = None,
+        substrate: _ReadSeekable,
+        asn1Spec: None,
+        tagSet: tag.TagSet | None = None,
         length: int | None = None,
         state: Unused = None,
         decodeFun: Unused = None,
         substrateFun: Unused = None,
-        **options,
-    ): ...
+        **options: dict[str, Any],  # `options` are passed to `codec.streaming.readFromStream()`
+    ) -> Generator[error.SubstrateUnderrunError | Literal[1, 0] | univ.Boolean]: ...
+    @overload
+    def valueDecoder(
+        self,
+        substrate: _ReadSeekable,
+        asn1Spec: _T,
+        tagSet: tag.TagSet | None = None,
+        length: int | None = None,
+        state: Unused = None,
+        decodeFun: Unused = None,
+        substrateFun: Unused = None,
+        **options: dict[str, Any],  # `options` are passed to `codec.streaming.readFromStream()`
+    ) -> Generator[error.SubstrateUnderrunError | Literal[1, 0] | _T]: ...
 
 BitStringPayloadDecoder = decoder.BitStringPayloadDecoder
 OctetStringPayloadDecoder = decoder.OctetStringPayloadDecoder
 RealPayloadDecoder = decoder.RealPayloadDecoder
 
-TAG_MAP: dict[TagSet, decoder.AbstractPayloadDecoder]
+TAG_MAP: dict[tag.TagSet, decoder.AbstractPayloadDecoder]
 TYPE_MAP: dict[int, decoder.AbstractPayloadDecoder]
 # deprecated aliases
 tagMap = TAG_MAP
 typeMap = TYPE_MAP
 
 class SingleItemDecoder(decoder.SingleItemDecoder):
-    TAG_MAP: dict[TagSet, decoder.AbstractPayloadDecoder]
+    TAG_MAP: dict[tag.TagSet, decoder.AbstractPayloadDecoder]
     TYPE_MAP: dict[int, decoder.AbstractPayloadDecoder]
 
 class StreamingDecoder(decoder.StreamingDecoder):
--- stubs/pyasn1/pyasn1/codec/cer/encoder.pyi
+++ stubs/pyasn1/pyasn1/codec/cer/encoder.pyi
@@ -1,31 +1,97 @@
-from typing import ClassVar
+from _typeshed import Unused
+from collections.abc import Iterable
+from typing import Any, ClassVar, Literal, Protocol, SupportsBytes, TypeVar, overload, type_check_only
 
 from pyasn1.codec.ber import encoder
-from pyasn1.type.tag import TagSet
+from pyasn1.type import base, tag, univ
 
 __all__ = ["Encoder", "encode"]
 
+_T = TypeVar("_T")
+_ValueT_contra = TypeVar("_ValueT_contra", contravariant=True)
+_SpecT_contra = TypeVar("_SpecT_contra", contravariant=True)
+
+@type_check_only
+class _EncodeFun(Protocol[_ValueT_contra, _SpecT_contra]):
+    def __call__(
+        self,
+        value: _ValueT_contra,
+        asn1Spec: _SpecT_contra,
+        **options: Any,  # `Any` as this may be a user-defined function
+    ) -> bytes: ...
+
 class BooleanEncoder(encoder.IntegerEncoder):
-    def encodeValue(self, value, asn1Spec, encodeFun, **options): ...
+    @overload
+    def encodeValue(
+        self, value: Literal[0, False], asn1Spec: Unused, encodeFun: Unused, **options: Unused
+    ) -> tuple[tuple[Literal[0]], Literal[False], Literal[False]]: ...
+    @overload
+    def encodeValue(
+        self, value: Literal[1, True], asn1Spec: Unused, encodeFun: Unused, **options: Unused
+    ) -> tuple[tuple[Literal[255]], Literal[False], Literal[False]]: ...
+    @overload
+    def encodeValue(
+        self,
+        value: Any,  # Technically, everything not equal to `0` will return `(255,), False, False`
+        asn1Spec: Unused,
+        encodeFun: Unused,
+        **options: Unused,
+    ) -> tuple[tuple[Literal[0, 255]], Literal[False], Literal[False]]: ...
 
 class RealEncoder(encoder.RealEncoder): ...
 
 class TimeEncoderMixIn:
-    Z_CHAR: ClassVar[int]
-    PLUS_CHAR: ClassVar[int]
-    MINUS_CHAR: ClassVar[int]
-    COMMA_CHAR: ClassVar[int]
-    DOT_CHAR: ClassVar[int]
-    ZERO_CHAR: ClassVar[int]
-    MIN_LENGTH: ClassVar[int]
-    MAX_LENGTH: ClassVar[int]
-    def encodeValue(self, value, asn1Spec, encodeFun, **options): ...
+    Z_CHAR: ClassVar[int] = 90
+    PLUS_CHAR: ClassVar[int] = 43
+    MINUS_CHAR: ClassVar[int] = 45
+    COMMA_CHAR: ClassVar[int] = 44
+    DOT_CHAR: ClassVar[int] = 46
+    ZERO_CHAR: ClassVar[int] = 48
+    MIN_LENGTH: ClassVar[int] = 12
+    MAX_LENGTH: ClassVar[int] = 19
 
-class GeneralizedTimeEncoder(TimeEncoderMixIn, encoder.OctetStringEncoder): ...
-class UTCTimeEncoder(TimeEncoderMixIn, encoder.OctetStringEncoder): ...
+    @overload
+    def encodeValue(
+        self,
+        value: univ.OctetString,
+        asn1Spec: None,
+        encodeFun: _EncodeFun[bytes, univ.OctetString],
+        **options: Any,  # `options` are passed to `encodeFun()`
+    ) -> tuple[bytes, bool, Literal[True]]: ...
+    @overload
+    def encodeValue(
+        self,
+        value: str | bytes | base.SimpleAsn1Type | SupportsBytes,
+        asn1Spec: univ.OctetString,
+        encodeFun: _EncodeFun[bytes, univ.OctetString],
+        **options: Any,  # `options` are passed to `encodeFun()`
+    ) -> tuple[bytes, bool, Literal[True]]: ...
+
+class GeneralizedTimeEncoder(TimeEncoderMixIn, encoder.OctetStringEncoder):
+    MIN_LENGTH: ClassVar[int] = 12
+    MAX_LENGTH: ClassVar[int] = 20
+
+class UTCTimeEncoder(TimeEncoderMixIn, encoder.OctetStringEncoder):
+    MIN_LENGTH: ClassVar[int] = 10
+    MAX_LENGTH: ClassVar[int] = 14
 
 class SetOfEncoder(encoder.SequenceOfEncoder):
-    def encodeValue(self, value, asn1Spec, encodeFun, **options): ...
+    @overload
+    def encodeValue(
+        self,
+        value: univ.SequenceOfAndSetOfBase,
+        asn1Spec: None,
+        encodeFun: _EncodeFun[base.Asn1Type | bytes, base.Asn1Type | None],
+        **options: Any,  # `options` are passed to `encodeFun()`
+    ) -> tuple[bytes, Literal[True], Literal[True]]: ...
+    @overload
+    def encodeValue(
+        self,
+        value: Iterable[_T],
+        asn1Spec: univ.SequenceOfAndSetOfBase,
+        encodeFun: _EncodeFun[_T | bytes, base.Asn1Type | None],
+        **options: Any,  # `options` are passed to `encodeFun()`
+    ) -> tuple[bytes, Literal[True], Literal[True]]: ...
 
 class SequenceOfEncoder(encoder.SequenceOfEncoder):
     def encodeValue(self, value, asn1Spec, encodeFun, **options): ...
@@ -36,7 +102,7 @@ class SetEncoder(encoder.SequenceEncoder):
 class SequenceEncoder(encoder.SequenceEncoder):
     omitEmptyOptionals: bool
 
-TAG_MAP: dict[TagSet, encoder.AbstractItemEncoder]
+TAG_MAP: dict[tag.TagSet, encoder.AbstractItemEncoder]
 TYPE_MAP: dict[int, encoder.AbstractItemEncoder]
 # deprecated aliases
 tagMap = TAG_MAP
@@ -46,7 +112,7 @@ class SingleItemEncoder(encoder.SingleItemEncoder):
     fixedDefLengthMode: bool
     fixedChunkSize: int
 
-    TAG_MAP: dict[TagSet, encoder.AbstractItemEncoder]
+    TAG_MAP: dict[tag.TagSet, encoder.AbstractItemEncoder]
     TYPE_MAP: dict[int, encoder.AbstractItemEncoder]
 
 class Encoder(encoder.Encoder):
